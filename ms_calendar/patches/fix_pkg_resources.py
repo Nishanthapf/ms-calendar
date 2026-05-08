c828f3bb40f97a0c401c4ba4713d37648744c38f
Mahaveer Ram P <mahaveer.p@azimpremjifoundation.org>
2026-05-08 12:45:37 +0530

    Bench Issuse
    Bench Issuse

--- ms_calendar/patches/fix_pkg_resources.py
+++ ms_calendar/patches/fix_pkg_resources.py
@@ -17,7 +17,20 @@ def execute():
 			except Exception:
 				return types.SimpleNamespace(version="0.0.0")
 
+		def resource_filename(package_or_requirement, resource_name):
+			import importlib.util
+			import os
+
+			try:
+				spec = importlib.util.find_spec(package_or_requirement)
+				if spec and spec.origin:
+					return os.path.join(os.path.dirname(spec.origin), resource_name)
+			except Exception:
+				pass
+			return resource_name
+
 		shim.get_distribution = get_distribution
+		shim.resource_filename = resource_filename
 		shim.DistributionNotFound = Exception
 		shim.require = lambda *args, **kwargs: None
 		sys.modules["pkg_resources"] = shim
